84d8d7e8cc4c796cc0853aa2e42495e8e0891ec5
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2024-09-04 09:45:22 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { UserProvider } from './contexts/UserContext'
+import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>
+          <Header />
+          <div className="container mx-auto mt-4">
+            {children}
+          </div>
+        </body>
+      </UserProvider>
     </html>
   )
 }
