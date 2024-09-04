6857d1200f942821f2369e92e2f5704a4f19da48
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2024-09-04 08:25:57 +0800

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'One Click CICD Login',
+  description: 'HSBC Staff Login Application',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
